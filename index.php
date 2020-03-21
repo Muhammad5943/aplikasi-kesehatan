db8be012121c29598496c32de70c468cd4a9bf88
Muhammad5943 <57320355+Muhammad5943@users.noreply.github.com>
2020-03-21 14:30:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,86 @@
-<h1>Ini adalah aplikasi heart beet detector</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 27</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <style>
+        body {
+
+            height: 950px;
+            background-color: black;
+
+        }
+    </style>
+</head>
+<body>
+    
+        <div class="container text-white">
+            <form action="#">
+                <div class="form-groub pt-5">
+                    <div class="col-md-12 text-center">
+                        <img src="lampung.jpg" class="rounded-circle" style="width: 20%; height: 20%;" alt="lampung.jpg">
+                    </div>
+
+                    <br><br>
+                    
+                    <div class="form-grup">
+                        <label class="col-md-12">
+                            Nama Lengkap
+                        </label>
+                        <div class="col-md-12">
+                            <input type="text" class="form-control" name="txtnama" placeholder="Nama Lengkap">
+                        </div>
+                    </div>
+
+                    <br><br>
+
+                    <div class="form-grup">
+                        <label class="col-md-12">
+                            Alamat
+                        </label>
+                        <div class="col-md-12">
+                            <textarea name="txtarea" class="form-control" cols="97" rows="2"></textarea>
+                        </div>
+                    </div>
+
+                    <br><br>
+                    
+                    <div class="form-grup">
+                        <label class="col-md-12">
+                            Alanat Email
+                        </label>
+                        <div class="col-md-12">
+                            <input type="email" class="form-control" name="txtemail" placeholder="Alamat Email">
+                        </div>
+                    </div>
+
+                    <br><br>
+
+                    <div class="form-grup">
+                        <label class="col-md-12">
+                            Password
+                        </label>
+                        <div class="col-md-12">
+                            <input type="password" class="form-control" name="txtpassword" placeholder="Password">
+                        </div>
+                    </div>
+
+                    <br><br>
+
+                    <div class="form-grup">
+                        <label for="#" class="col-md-12">
+                            Scan KTP
+                        </label>
+                        <div class="col-md-12">
+                            <input type="file" class="form-control" name="txtnama">
+                        </div>
+                    </div>
+
+
+                </div>
+            </form>
+        </div>
+    </div>
+</html>
